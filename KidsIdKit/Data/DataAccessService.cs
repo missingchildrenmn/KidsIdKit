81bd435ecd4cb774a76e787c1f73f2b084ebe4f2
Rockford Lhotka <rocky@lhotka.net>
2023-02-17 22:10:26 -0800

    Clearly specify backup file/path

--- KidsIdKit/Data/DataAccessService.cs
+++ KidsIdKit/Data/DataAccessService.cs
@@ -5,8 +5,10 @@ namespace KidsIdKit.Data
 {
   public class DataAccessService : IDataAccess
   {
-    private string fileName = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)
-      + Path.DirectorySeparatorChar + "kidsidkitdata.txt";
+    private string fileName = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData)
+      + Path.DirectorySeparatorChar + "kidsidkitdata.dat";
+    private string backupFileName = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)
+      + Path.DirectorySeparatorChar + "kidsidkitdata.bak";
 
     public Family GetData()
     {
@@ -32,7 +34,7 @@ namespace KidsIdKit.Data
     public void SaveData(Family data)
     {
       if (File.Exists(fileName))
-        File.Copy(fileName, fileName + ".bak");
+        File.Copy(fileName, backupFileName);
       var json = JsonSerializer.Serialize(data);
       // TODO: add encryption
       File.WriteAllText(fileName, json);
