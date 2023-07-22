abb1f94e6e79dbe664a56fc964675967b5b6779d
Rockford Lhotka <rocky@lhotka.net>
2023-07-22 11:16:14 -0400

    Add bootstrap theme customization

---
+++ KidsIdKit/Services/IGetText.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KidsIdKit.Services
+{
+    public interface IGetText
+    {
+        string GetText();
+    }
+}
