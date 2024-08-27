bf7cd397ae90bae41cb6fa8d8f376dab9578338f
abergquist <abergquist@sbcglobal.net>
2024-08-26 18:46:13 -0700

    Extracted GetPhoneLink() method into HyperlinkHelper.cs

--- KidsIdKit/Pages/info/HyperlinkHelper.cs
+++ KidsIdKit/Pages/info/HyperlinkHelper.cs
@@ -13,5 +13,17 @@ namespace KidsIdKit.Pages.info
         {
             return (MarkupString)$"<a href='mailto:{emailAddress}'>{emailAddress}</a>";
         }
+
+        public static MarkupString GetPhoneLink(string cpiapStateDepartment)
+        {
+            var callablePhoneNumber = CallablePhoneNumber(cpiapStateDepartment);
+            var phoneLink = $"<a href='tel:{callablePhoneNumber}'>{cpiapStateDepartment}</a>";
+            return (MarkupString)phoneLink;
+        }
+
+        private static string CallablePhoneNumber(string input)
+        {
+            return "+1" + new string(input.Where(char.IsDigit).ToArray());
+        }
     }
 }
