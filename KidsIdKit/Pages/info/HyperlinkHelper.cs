515fa8f578fea364f6df59d6c16dc990daaf5ce4
abergquist <abergquist@sbcglobal.net>
2024-08-26 18:23:32 -0700

    Moved SetEmailLink() method out into a new HyperlinkHelper.cs program

---
+++ KidsIdKit/Pages/info/HyperlinkHelper.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KidsIdKit.Pages.info
+{
+    public static class HyperlinkHelper
+    {
+        public static MarkupString SetEmailLink(string emailAddress)
+        {
+            return (MarkupString)$"<a href='mailto:{emailAddress}'>{emailAddress}</a>";
+        }
+    }
+}
