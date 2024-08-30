192abad6837ac80755b50e5e9234b3fd6c2457dc
Art Bergquist <abergquist@sbcglobal.net>
2024-08-30 10:49:01 -0700

    15-Implemented new International Abduction page plus other cleanup (#24)
    * Initial implementation of the International Abductions page
    
    * Eliminated duplication of "(888) 407-4747" and made both occurrences callable hyperlinks.
    
    * Extracted both occurrences of "<a href='tel:@callablePhoneNumber'>@cpiapStateDepartment</a>" into a phoneLink variable
    
    * Extracted duplicate "preventabduction@state.gov" text to a new email variable.
    
    * Extracted code to a new SetEmailLink() method.
    
    * Moved SetEmailLink() method out into a new HyperlinkHelper.cs program
    
    * Extracted code to a new GetPhoneLink() method
    
    * 1. Cleaned up unnecessary, commented-out code
    2. Replaced string with var
    
    * Extracted phoneLink to its own variable
    
    * Extracted GetPhoneLink() method into HyperlinkHelper.cs
    
    * Moved code into HyperlinkHelper.cs
    
    * Extracted both occurrences of "AMECO at @phoneLink for the AMECO-affiliated organization nearest you." to a new PhoneLinkComponent
    
    * Grouped phone number-related methods into a new PhoneNumberHelper inner class.

---
+++ KidsIdKit/Pages/info/HyperlinkHelper.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace KidsIdKit.Pages.info
+{
+    public static class HyperlinkHelper
+    {
+        public static MarkupString SetEmailLink(string emailAddress)
+        {
+            return (MarkupString)$"<a href='mailto:{emailAddress}'>{emailAddress}</a>";
+        }
+
+        public class PhoneNumberHelper
+        {
+            public static MarkupString GetPhoneLink(string cpiapStateDepartment)
+            {
+                var callablePhoneNumber = CallablePhoneNumber(cpiapStateDepartment);
+                var phoneLink = $"<a href='tel:{callablePhoneNumber}'>{cpiapStateDepartment}</a>";
+                return (MarkupString)phoneLink;
+            }
+
+            private static string CallablePhoneNumber(string input)
+            {
+                return "+1" + new string(input.Where(char.IsDigit).ToArray());
+            }
+        }
+    }
+}
