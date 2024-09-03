e9affc91e2a2ea480a3496bb62d19aae63a9a77a
abergquist <abergquist@sbcglobal.net>
2024-09-03 07:25:24 -0700

    1. Initial commit of new "Definition of a Runaway Youth" page. 2. Extracted duplicate code to a new RunawayPhoneNumber() method 3. Refactoring

--- KidsIdKit/Pages/info/InfoHyperlink.cs
+++ KidsIdKit/Pages/info/InfoHyperlink.cs
@@ -9,9 +9,15 @@ namespace KidsIdKit.Pages.info
 {
     public class InfoHyperlink
     {
+        public static MarkupString RunawayPhoneNumber() {
+            var userFriendlyPhoneLink = HyperlinkHelper.PhoneNumberHelper.GetPhoneLink("1-800-786-2929");
+            return (MarkupString)$"1-888-RUNAWAY ({userFriendlyPhoneLink})";
+        }
+
         public static MarkupString RunYellPhoneNumber() {
-            var userFriendlyPhoneLink = HyperlinkHelper.PhoneNumberHelper.GetPhoneLink("1-888-786-9355");
-            return (MarkupString)$"1-888-RUN-YELL ({userFriendlyPhoneLink})";
+            var userFriendly800PhoneLink = HyperlinkHelper.PhoneNumberHelper.GetPhoneLink("1-888-786-9355");
+            var userFriendlyPhoneLink = HyperlinkHelper.PhoneNumberHelper.GetPhoneLink("1-612-334-9449");
+            return (MarkupString)$"{userFriendlyPhoneLink} or 1-888-RUN-YELL ({userFriendly800PhoneLink})";
         }
     }
 }
