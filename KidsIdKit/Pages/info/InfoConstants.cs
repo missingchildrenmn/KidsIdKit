98ee0c77aa18d13b6e360d23924ce1933246f3e2
Art Bergquist <abergquist@sbcglobal.net>
2024-09-02 21:02:52 -0700

    15-missing plus refactorings (#25)
    * Replaced the text "here" with a hyperlink in both occurrences of "MN Statute details are here"; as a result, the Abduction web page is more user-friendly (because it no longer shows the details of the 'MN Statute details' URL).
    
    * Genericized the parameter for the GetPhoneLink() method.
    
    * "800-843-5678" is now a telephone hyperlink in the Abduction web page
    
    * Replaced hyperlink with call to new Link() method.
    
    * Simplified the link in the Abouthtbox page by employing a new HtmlLink() method in HyperlinkHelper.cs.
    
    * Implemented a new inner class in HyperlinkHelper.cs to group together general link methods.
    
    * Inlined SetEmailLink() method call.
    
    * More refactoring
    
    * Initial commit of new Missing.razor file
    
    * Extracted emDash to a constant in a new InfoConstants.cs file.
    
    * Extracted duplicate code to a new RunYellPhoneNumber() method in a new InfoHyperlink.cs.
    
    * Made Phone # clickable and some refactorings.

---
+++ KidsIdKit/Pages/info/InfoConstants.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KidsIdKit.Pages.info
+{
+    public static class InfoConstants
+    {
+        public static readonly MarkupString EM_DASH = (MarkupString)"&mdash;";
+    }
+}
