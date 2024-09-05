e7daa2a391fe460a4b9e55e007cb464d04ca13d2
abergquist <abergquist@sbcglobal.net>
2024-09-04 18:29:36 -0700

    Implemented new Safety page.

--- KidsIdKit/Pages/info/InfoConstants.cs
+++ KidsIdKit/Pages/info/InfoConstants.cs
@@ -10,5 +10,6 @@ namespace KidsIdKit.Pages.info
     public static class InfoConstants
     {
         public static readonly MarkupString EM_DASH = (MarkupString)"&mdash;";
+        public static readonly MarkupString EN_DASH = (MarkupString)"&ndash;";
     }
 }
