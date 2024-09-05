dca269a083d32e404a4d098f05854a03a98a6c82
Art Bergquist <abergquist@sbcglobal.net>
2024-09-05 07:44:04 -0700

    15-safety (#28)
    * Implemented new Safety page.
    
    * Replaced all the Links in the Info page with a data-driven approach.
    
    * Couple refactorings

--- KidsIdKit/Pages/info/InfoConstants.cs
+++ KidsIdKit/Pages/info/InfoConstants.cs
@@ -1,14 +1,11 @@
 ﻿using Microsoft.AspNetCore.Components;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace KidsIdKit.Pages.info
 {
     public static class InfoConstants
     {
+        public static readonly MarkupString DOUBLE_QUOTE = (MarkupString)"&quote;";
         public static readonly MarkupString EM_DASH = (MarkupString)"&mdash;";
+        public static readonly MarkupString EN_DASH = (MarkupString)"&ndash;";
     }
 }
