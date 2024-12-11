d1378640894f72e272abeed7147bdb7819368f1e
Rockford lhotka <rocky@lhotka.net>
2024-12-11 16:05:35 -0600

    Use more agressive PWA reload techniques

--- KidsIdKit.Web/wwwroot/service-worker.published.js
+++ KidsIdKit.Web/wwwroot/service-worker.published.js
@@ -19,6 +19,9 @@ const manifestUrlList = self.assetsManifest.assets.map(asset => new URL(asset.ur
 async function onInstall(event) {
     console.info('Service worker: Install');
 
+    // Activate the new service worker as soon as the old one is retired.
+    self.skipWaiting();
+
     // Fetch and cache all matching items from the assets manifest
     const assetsRequests = self.assetsManifest.assets
         .filter(asset => offlineAssetsInclude.some(pattern => pattern.test(asset.url)))
