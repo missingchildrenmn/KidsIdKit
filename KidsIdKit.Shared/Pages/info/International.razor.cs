107fae39eb9917f31bf30dbfd891a5ad54d864d5
abergquist <abergquist@sbcglobal.net>
2025-03-13 20:20:05 -0700

    Refactor International component for better organization
    Separated logic and markup into International.razor and
    International.razor.cs. Moved phone number handling to
    the code-behind file to improve maintainability and clarity.

---
+++ KidsIdKit.Shared/Pages/info/International.razor.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace KidsIdKit.Shared.Pages.info
+{
+    public partial class International : ComponentBase
+    {
+        private string cpiapStateDepartmentPhoneNumber = "(888) 407-4747";
+        private MarkupString? phoneLink;
+
+        protected override void OnInitialized()
+        {
+            phoneLink = HyperlinkHelper.PhoneNumberHelper.GetPhoneLink(cpiapStateDepartmentPhoneNumber);
+        }
+    }
+}
