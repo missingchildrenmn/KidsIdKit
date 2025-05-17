6e33ad2344c851fcbae1e7f4efc30b7a49776cf6
abergquist <abergquist@sbcglobal.net>
2025-05-17 12:13:26 -0700

    Update phone number references in International component
    Replaced the `phoneLink` variable with `cpiapStateDepartmentPhoneNumberLink` in `International.razor` for clarity. Updated the initialization in `International.razor.cs` to reflect this change, ensuring consistent usage related to the Children's Passport Issuance Alert Program (CPIAP).

--- KidsIdKit.Shared/Pages/info/International.razor.cs
+++ KidsIdKit.Shared/Pages/info/International.razor.cs
@@ -5,11 +5,11 @@ namespace KidsIdKit.Shared.Pages.info
     public partial class International : ComponentBase
     {
         private string cpiapStateDepartmentPhoneNumber = "(888) 407-4747";
-        private MarkupString? phoneLink;
+        private MarkupString? cpiapStateDepartmentPhoneNumberLink;
 
         protected override void OnInitialized()
         {
-            phoneLink = HyperlinkHelper.PhoneNumberHelper.GetPhoneLink(cpiapStateDepartmentPhoneNumber);
+            cpiapStateDepartmentPhoneNumberLink = HyperlinkHelper.PhoneNumberHelper.GetPhoneLink(cpiapStateDepartmentPhoneNumber);
         }
     }
 }
