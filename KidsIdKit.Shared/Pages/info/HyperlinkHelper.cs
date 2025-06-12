4d7d827f30f1ab3b781f3376528cc7f9a14bb58a
Art Bergquist <abergquist@sbcglobal.net>
2025-06-12 00:44:43 -0700

    Add phone icons to contact links for better visibility (#78)
    Updated various files to include phone icons next to contact numbers, enhancing the visual representation and user experience. Changes made in `HyperlinkHelper.cs`, `Safety.razor`, `AbductionTests.razor`, `AboutmcmTests.razor`, `InternationalTests.razor`, `MissingTests.razor`, and `RunawayTests.razor`.

--- KidsIdKit.Shared/Pages/info/HyperlinkHelper.cs
+++ KidsIdKit.Shared/Pages/info/HyperlinkHelper.cs
@@ -13,25 +13,18 @@ namespace KidsIdKit.Shared.Pages.info
 
         public class PhoneNumberHelper
         {
-            public static MarkupString GetPhoneLink(string userFriendlyPhoneNumber, string realPhoneNumber)
+            public static MarkupString GetPhoneIcon()
             {
-                var callablePhoneNumber = CallablePhoneNumber(realPhoneNumber);
-                var phoneLink = $"<a href='tel:{callablePhoneNumber}'>{userFriendlyPhoneNumber}</a>";
-                return (MarkupString)phoneLink;
+                string phoneIcon = "<i class='fa-solid fa-phone'></i>";
+                return (MarkupString)phoneIcon;
             }
 
             public static MarkupString GetPhoneLink(string userFriendlyPhoneNumber)
             {
                 var callablePhoneNumber = CallablePhoneNumber(userFriendlyPhoneNumber);
-                var phoneLink = $"<a href='tel:{callablePhoneNumber}'>{userFriendlyPhoneNumber}</a>";
+                var phoneLink = $"{GetPhoneIcon()} <a href='tel:{callablePhoneNumber}'>{userFriendlyPhoneNumber}</a>";
                 return (MarkupString)phoneLink;
-                //return GetPhoneLink(userFriendlyPhoneNumber, callablePhoneNumber);
             }
-
-            //public static MarkupString GetPhoneLink(string numericOnlyPhoneNumber, string vanityPhoneNumber) {
-            //    var phoneLink = LinkHelper.Link("tel", vanityPhoneNumber, numericOnlyPhoneNumber);
-            //    return (MarkupString)phoneLink;
-            //}
 
             private static string CallablePhoneNumber(string input)
             {
@@ -47,13 +40,11 @@ namespace KidsIdKit.Shared.Pages.info
                 return link;
             }
 
-            // TODO: Art note: I believe the following Link method can be removed ...
             public static string Link(string text, string url)
             {
                 return Link(String.Empty, text, url);
             }
 
-            // Art note: ... and the 2 references to "type" can be removed from the following Link() method.
             private static string Link(string type, string text, string url)
             {
                 var link = $"<a href='{type}{(String.IsNullOrEmpty(type) ? String.Empty : ":")}{url}'>{text}</a>";
@@ -61,7 +52,7 @@ namespace KidsIdKit.Shared.Pages.info
             }
         }
 
-        public static string MinnesotaStateStatuteDetailsUrl(string minnesotaStateStatute)                     // https://www.revisor.mn.gov/statutes/?id=nnn.nn[n]
+        public static string MinnesotaStateStatuteDetailsUrl(string minnesotaStateStatute)
         {
             return $"{minnesotaLegislatureOfficeOfTheRevisorOfStatutesUrl}?id={minnesotaStateStatute}";
         }
