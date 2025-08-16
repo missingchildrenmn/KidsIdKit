4daa0f9c13ce28b6c69e22728f68e5d9a0875a14
abergquist <abergquist@sbcglobal.net>
2025-08-15 17:45:38 -0700

    Add properties to Family and PhysicalDetails classes
    - Added a blank line in the Family class and blank lines in the PhysicalDetails class for readability.

--- KidsIdKit.Shared/Data/PhysicalDetails.cs
+++ KidsIdKit.Shared/Data/PhysicalDetails.cs
@@ -5,24 +5,35 @@ namespace KidsIdKit.Data
     public class PhysicalDetails
     {
         public string? Height { get; set; }
+
         public string? Weight { get; set; }
+
         [Display(Name = "Measurement date")]
         public DateTime MeasurementDate { get; set; } = DateTime.Today;
+
         [Display(Name = "Hair color")]
         public string? HairColor { get; set; }
+
         [Display(Name = "Hair style")]
         public string? HairStyle { get; set; }
+
         [Display(Name = "Eye color")]
         public string? EyeColor { get; set; }
+
         [Display(Name = "Eye glasses")]
         public bool EyeGlasses { get; set; }
+
         [Display(Name = "Wears contacts")]
         public bool EyeContacts { get; set; }
+
         [Display(Name = "Skin tone")]
         public string? SkinTone { get; set; }
+
         [Display(Name = "Racial/ethnic identity")]
         public string? RacialEthnicIdentity { get; set; }
+
         public string? Sex { get; set; }
+
         [Display(Name = "Gender identity")]
         public string? GenderIdentity { get; set; }
     }
