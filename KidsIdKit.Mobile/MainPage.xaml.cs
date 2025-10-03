fc80946c0bdf60db8e9b80312e8fd6c920d7b2be
Art Bergquist <abergquist@sbcglobal.net>
2025-10-03 15:02:19 -0700

    Pre 2025 10 04 demo enhancements (#92)
    * In lieu of just the First Name, now displayed the Full Name in all the "sub-pages".
    
    * When running the app on an Android phone, now placing the app below the Android status bar.

--- KidsIdKit.Mobile/MainPage.xaml.cs
+++ KidsIdKit.Mobile/MainPage.xaml.cs
@@ -6,4 +6,40 @@ public partial class MainPage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    protected override void OnHandlerChanged()
+    {
+        base.OnHandlerChanged();
+
+#if ANDROID
+        PlaceAppBelowAndroidStatusBar();
+
+        void PlaceAppBelowAndroidStatusBar()
+        {
+            Android.Views.View? mauiView = this.Handler?.PlatformView as Android.Views.View;
+            if (mauiView != null)
+            {
+                int statusBarHeightPx = 0;
+                var androidResources = mauiView.Context.Resources;
+                int resourceId = androidResources.GetIdentifier("status_bar_height", "dimen", "android");
+                if (resourceId > 0)
+                {
+                    statusBarHeightPx = androidResources.GetDimensionPixelSize(resourceId);
+                }
+
+                // Convert pixels to device-independent units (dp)
+                float density = androidResources.DisplayMetrics.Density;
+                double statusBarHeightDp = statusBarHeightPx / density;
+
+                // Optionally add a small offset for visual comfort
+                double appBarOffset = statusBarHeightDp + 8;
+
+                if (this.Padding.Top != appBarOffset)
+                {
+                    this.Padding = new Thickness(0, appBarOffset, 0, 0);
+                }
+            }
+        }
+#endif
+    }
 }
