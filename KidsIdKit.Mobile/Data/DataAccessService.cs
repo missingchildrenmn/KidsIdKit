dc568b3d2a65a425851cc8a91d72239651c2edfe
abergquist <abergquist@sbcglobal.net>
2025-10-06 20:51:18 -0700

    Extracted code to create a new family in DataAccessService.cs.

--- KidsIdKit.Mobile/Data/DataAccessService.cs
+++ KidsIdKit.Mobile/Data/DataAccessService.cs
@@ -4,9 +4,22 @@ namespace KidsIdKit.Data
 {
     public class DataAccessService : IDataAccess
     {
-        private static readonly string localApplicationDataFolder = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + Path.DirectorySeparatorChar;
-        private readonly string fileName = localApplicationDataFolder + "kidsidkitdata.dat";
-        private readonly string backupFileName = localApplicationDataFolder + "kidsidkitdata.bak";
+        // TODO: Could use reflection to derive the name of the project
+        private const string projectName = "kidsidkitdata";
+
+        //-----------------------------------------------------------------------------------------------------------------------------------
+        // An enumeration member in the.NET framework's System.Environment class. It represents a special folder on the user's system
+        // specifically designated for storing application-specific data that is local to the current user and not intended to roam
+        // between different machines or profiles.
+        //
+        // The directory that serves as a common repository for application-specific data for the current, non-roaming user.
+        private const Environment.SpecialFolder appSpecificDataDirForCurrentNonRoamingUser = Environment.SpecialFolder.LocalApplicationData;
+        //-----------------------------------------------------------------------------------------------------------------------------------
+        private static readonly string localApplicationDataFolder = Environment.GetFolderPath(appSpecificDataDirForCurrentNonRoamingUser) +
+                                                                    Path.DirectorySeparatorChar;
+
+        private readonly string fileName = localApplicationDataFolder + projectName + ".dat";
+        private readonly string backupFileName = localApplicationDataFolder + projectName + ".bak";
 
         public Task<Family?> GetDataAsync()
         {
@@ -20,10 +33,15 @@ namespace KidsIdKit.Data
                 }
                 else
                 {
-                    return Task.FromResult<Family?>(new Family());
+                    return NewFamily();
                 }
             }
             catch
+            {
+                return NewFamily();
+            }
+
+            static Task<Family?> NewFamily()
             {
                 return Task.FromResult<Family?>(new Family());
             }
