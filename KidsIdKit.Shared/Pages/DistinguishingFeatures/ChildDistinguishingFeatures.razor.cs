51bd41dc3a49d09136f7cda07938b33dbbc54fc7
abergquist <abergquist@sbcglobal.net>
2025-11-03 14:07:46 -0800

    Moved ability to edit to the "card" (no visible aspect of a card yet - e.g., borders).

--- KidsIdKit.Shared/Pages/DistinguishingFeatures/ChildDistinguishingFeatures.razor.cs
+++ KidsIdKit.Shared/Pages/DistinguishingFeatures/ChildDistinguishingFeatures.razor.cs
@@ -18,4 +18,21 @@ public partial class ChildDistinguishingFeatures
         CurrentChild = DataStore.Family.Children[Id].ChildDetails;
         Features = DataStore.Family.Children[Id].DistinguishingFeatures.AsQueryable();
     }
+
+    private void NavigateToEdit(DistinguishingFeature feature)
+    {
+        NavigationManager.NavigateTo($"/Feature/{Id}/{feature.Id}");
+    }
+
+    //private void NavigateToEdit(Data.Child child)
+    //{
+    //    if (DataStore.Family != null)
+    //    {
+    //        var index = DataStore.Family.Children.IndexOf(child);
+    //        //NavigationManager.NavigateTo($"/Child/{index}");
+    //        NavigationManager.NavigateTo($"/Feature/@Id/@context.Id");
+    //    }
+    //}
 }
+
+//<a href = "/Feature/@Id/@context.Id" style="text-decoration:none"><i class="fas fa-edit"></i></a>
