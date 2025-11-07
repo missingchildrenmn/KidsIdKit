0b6b4020226bdad0898dc6e39cc637e979d0f81b
abergquist <abergquist@sbcglobal.net>
2025-11-07 10:09:38 -0800

    Ability to toggle password/visibility in SocialMediaAccounts page

--- KidsIdKit.Shared/Pages/SocialMediaAccounts/SocialMediaAccounts.razor.cs
+++ KidsIdKit.Shared/Pages/SocialMediaAccounts/SocialMediaAccounts.razor.cs
@@ -9,6 +9,8 @@ public partial class SocialMediaAccounts
 
     ChildDetails? CurrentChild { get; set; }
     IEnumerable<SocialMediaAccount>? SocialMediaAccountObjects;
+    readonly string PageTitle = "Social Media Accounts";
+    private readonly PasswordVisibilityManager passwordVisibilityManager = new();
 
     protected override void OnParametersSet()
     {
@@ -17,5 +19,26 @@ public partial class SocialMediaAccounts
             CurrentChild = DataStore.Family.Children[Id].ChildDetails;
             SocialMediaAccountObjects = DataStore.Family.Children[Id].SocialMediaAccounts;
         }
+    }
+
+    private class PasswordVisibilityManager
+    {
+        private readonly HashSet<int> visiblePasswords = new();
+
+        public void ShowPassword(int accountId) => visiblePasswords.Add(accountId);
+
+        public void HidePassword(int accountId) => visiblePasswords.Remove(accountId);
+
+        public bool IsPasswordVisible(int accountId) => visiblePasswords.Contains(accountId);
+
+        public void TogglePassword(int accountId)
+        {
+            if (!visiblePasswords.Remove(accountId))
+                visiblePasswords.Add(accountId);
+        }
+
+        public void HideAllPasswords() => visiblePasswords.Clear();
+
+        public int VisiblePasswordCount => visiblePasswords.Count;
     }
 }
