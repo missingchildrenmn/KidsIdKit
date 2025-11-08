e1f9e9f4a9346f9c6c60a50d2f343b9f58350e37
Art Bergquist <abergquist@sbcglobal.net>
2025-11-07 22:34:06 -0800

    103 - Social media accounts (#104)
    * Edited the ReadMe.md file.
    
    * Extracted code to create a new family in DataAccessService.cs.
    
    * The JSON .dat file is now stored in a .Zip file.
    
    * Made the Kids page look better.
    
    * Implemented a new PageTitle property to eliminate duplication and ensure consistency.
    
    * Implemented a new PageTitle property to eliminate duplication and ensure consistency.
    
    * Eliminated duplication as well as other enhancements.
    
    * Moved ability to edit to the "card" (no visible aspect of a card yet - e.g., borders).
    
    * All distinguishing features are now in 1 big (tall) card.
    
    * Each distinguishing feature is now in its own card.
    
    * The delete button (trash can icon) now always appears in the lower-right corner of the card.
    
    * No longer showing the table header if there are no records/rows in the table.
    
    * New Social Media feature.
    
    * Implemented ability to hide (the default) a password or show a password for each social media account.
    
    * Ability to toggle password/visibility in SocialMediaAccounts page
    
    * Implemented a new [Hide all passwords] button at the bottom (just after the last row) of the 'Password' column.
    
    * 'Password' in SocialMediaAccountDetails.razor can now be masked or unmasked.
    
    * Subsumed EditPasswordRow.razor functionality into an enhanced EditTextRow.razor (and EditText.razor).

---
+++ KidsIdKit.Shared/Data/SocialMediaAccount.cs
@@ -0,0 +1,21 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace KidsIdKit.Shared.Data;
+
+public class SocialMediaAccount
+{
+    public int Id { get; set; }
+
+    [Required]
+    [Display(Name = "Platform")]
+    public string? Platform { get; set; }
+
+    [Required]
+    [Display(Name = "User Name")]
+    public string UserName { get; set; }
+
+    [Required]
+    [Display(Name = "Password")]
+    public string Password { get; set; }
+}
