0aa40036a5b0cd14d744d5a40321714c0e14e316
abergquist <abergquist@sbcglobal.net>
2026-04-24 14:35:56 -0700

    Update constants, UI text, and add info card
    Refactor Abductions.razor to use InfoConstants for apostrophes and quotation marks, and update text to reference missingChild variable. Add APOSTROPHE, EM_DASH, and EN_DASH constants in InfoConstants.cs.

--- KidsIdKit.Core/Pages/Information/InfoConstants.cs
+++ KidsIdKit.Core/Pages/Information/InfoConstants.cs
@@ -1,12 +1,15 @@
+using KidsIdKit.Core.Pages.Information;
 using Microsoft.AspNetCore.Components;
 
 namespace KidsIdKit.Core.Pages.Information
 {
     public static class InfoConstants
     {
+        public static readonly MarkupString APOSTROPHE = (MarkupString)"&rsquo;";
         public static readonly MarkupString DOUBLE_QUOTE = (MarkupString)"&quote;";
         public static readonly MarkupString LEFT_DOUBLE_QUOTATION_MARK = (MarkupString)"&ldquo;";
         public static readonly MarkupString RIGHT_DOUBLE_QUOTATION_MARK = (MarkupString)"&rdquo;";
+
         public static readonly MarkupString EM_DASH = (MarkupString)"&mdash;";
         public static readonly MarkupString EN_DASH = (MarkupString)"&ndash;";
     }
