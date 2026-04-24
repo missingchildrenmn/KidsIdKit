91bee278428c78fccbd376b5d809c53fcfbf5c59
abergquist <abergquist@sbcglobal.net>
2026-04-24 15:25:48 -0700

    Standardize possessive forms with MakePossessive utility
    Refactored Razor files to use InfoConstants.MakePossessive for consistent possessive word formatting using a right single quotation mark. Improved text consistency, readability, and maintainability across informational pages. Minor content clarifications and formatting adjustments included.

--- KidsIdKit.Core/Pages/Information/InfoConstants.cs
+++ KidsIdKit.Core/Pages/Information/InfoConstants.cs
@@ -5,12 +5,17 @@ namespace KidsIdKit.Core.Pages.Information
 {
     public static class InfoConstants
     {
-        public static readonly MarkupString APOSTROPHE = (MarkupString)"&rsquo;";
+        public static readonly MarkupString APOSTROPHE = (MarkupString)"&rsquo;";   // Right single quotation mark
         public static readonly MarkupString DOUBLE_QUOTE = (MarkupString)"&quote;";
         public static readonly MarkupString LEFT_DOUBLE_QUOTATION_MARK = (MarkupString)"&ldquo;";
         public static readonly MarkupString RIGHT_DOUBLE_QUOTATION_MARK = (MarkupString)"&rdquo;";
 
         public static readonly MarkupString EM_DASH = (MarkupString)"&mdash;";
         public static readonly MarkupString EN_DASH = (MarkupString)"&ndash;";
+
+        public static MarkupString MakePossessive(string text)
+        {
+            return (MarkupString)$"{text}{APOSTROPHE}s";
+        }
     }
 }
