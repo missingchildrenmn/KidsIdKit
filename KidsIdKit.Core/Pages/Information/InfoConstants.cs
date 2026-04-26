0b3f8b24fd8b69181869c3ad4081fb6597b3c797
abergquist <abergquist@sbcglobal.net>
2026-04-25 19:37:59 -0700

    Add safety tips and trademark symbol to info pages
    Updated InfoConstants with REGISTERED_TRADE_MARK_SIGN for ®. Updated "Erica's Choices" in Safety.razor to include registered the trademark symbol as an HTML entity (so that it won't appear as a white question mark in a black diamond).

--- KidsIdKit.Core/Pages/Information/InfoConstants.cs
+++ KidsIdKit.Core/Pages/Information/InfoConstants.cs
@@ -7,11 +7,11 @@ namespace KidsIdKit.Core.Pages.Information
     {
         public static readonly MarkupString APOSTROPHE = (MarkupString)"&rsquo;";   // Right single quotation mark
         public static readonly MarkupString DOUBLE_QUOTE = (MarkupString)"&quote;";
-        public static readonly MarkupString LEFT_DOUBLE_QUOTATION_MARK = (MarkupString)"&ldquo;";
-        public static readonly MarkupString RIGHT_DOUBLE_QUOTATION_MARK = (MarkupString)"&rdquo;";
-
         public static readonly MarkupString EM_DASH = (MarkupString)"&mdash;";
         public static readonly MarkupString EN_DASH = (MarkupString)"&ndash;";
+        public static readonly MarkupString LEFT_DOUBLE_QUOTATION_MARK = (MarkupString)"&ldquo;";
+        public static readonly MarkupString REGISTERED_TRADE_MARK_SIGN = (MarkupString)"&reg;";
+        public static readonly MarkupString RIGHT_DOUBLE_QUOTATION_MARK = (MarkupString)"&rdquo;";
 
         public static MarkupString Append_apostrophe_and_s_to(string text)
         {
