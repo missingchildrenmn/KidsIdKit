fb08680c77ff7f00620a9972f45f6562e4e3535d
Kevin E. Ford <4380198+Bowman74@users.noreply.github.com.>
2026-05-24 10:58:12 -0700

    Change the iOS app's tintcolor to match the color palet for the application.

--- KidsIdKit.Mobile/Platforms/iOS/AppDelegate.cs
+++ KidsIdKit.Mobile/Platforms/iOS/AppDelegate.cs
@@ -1,9 +1,16 @@
 using Foundation;
+using UIKit;
 
 namespace KidsIdKit;
 
 [Register("AppDelegate")]
 public class AppDelegate : MauiUIApplicationDelegate
 {
+    public override void OnActivated(UIApplication application)
+    {
+        UIView.Appearance.TintColor = UIColor.FromRGB(94, 94, 94);
+        base.OnActivated(application);
+    }
+
 	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
 }
